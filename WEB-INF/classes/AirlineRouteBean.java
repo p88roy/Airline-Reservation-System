f58deb53e47b15d424e82236ac80212afbe19e92
Pratibha Roy <42288133+p88roy@users.noreply.github.com>
2019-05-26 12:06:46 +0530

    Create AirlineRouteBean.java

---
+++ WEB-INF/classes/AirlineRouteBean.java
@@ -0,0 +1,42 @@
+public class AirlineRouteBean
+{
+ private String routeCode;
+ private String flightFrom;
+ private String flightTo;
+ private String frequency;
+
+ public AirlineRouteBean() {
+ }
+ 
+ public void setRouteCode(String aRouteCode) {
+   routeCode= aRouteCode;
+}
+ public String getRouteCode() {
+   return routeCode;
+}
+
+
+public void setFlightFrom(String aFlightFrom) {
+   flightFrom= aFlightFrom;
+}
+ public String getFlightFrom() {
+   return flightFrom;
+}
+
+
+public void setFlightTo(String aFlightTo) {
+   flightTo= aFlightTo;
+}
+ public String getFlightTo() {
+   return flightTo;
+}
+
+
+public void setFrequency(String aFrequency) {
+   frequency= aFrequency;
+}
+ public String getFrequency() {
+   return frequency;
+}
+
+}
