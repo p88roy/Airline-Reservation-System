e8e2373063743992e160816a750f130098e17227
Pratibha Roy <42288133+p88roy@users.noreply.github.com>
2019-05-26 12:06:19 +0530

    Create AirlineBookingBean.java

---
+++ WEB-INF/classes/AirlineBookingBean.java
@@ -0,0 +1,151 @@
+public class AirlineBookingBean
+{
+ private String bookingId;
+ private String routeCode;
+ private String airlineCode;
+ private String airlineDate;
+ private String passengerName;
+ private String gender;
+ private String dateOfBirth;
+ private String fare;
+ private String departureTime;
+ private String seatClass;
+ private String mobileNumber;
+ private String paymentOption;
+ private String cardNo;
+ private String cvvNo;
+ private String chequeNo;
+ private String address;
+ private String notes;
+
+ public AirlineBookingBean() {
+ }
+ 
+ public void setBookingId(String aBookingId) {
+   bookingId= aBookingId;
+}
+ public String getBookingId() {
+   return bookingId;
+}
+
+
+public void setRouteCode(String aRouteCode) {
+   routeCode= aRouteCode;
+}
+ public String getRouteCode() {
+   return routeCode;
+}
+
+
+public void setAirlineCode(String aAirlineCode) {
+   airlineCode= aAirlineCode;
+}
+ public String getAirlineCode() {
+   return airlineCode;
+}
+
+
+public void setAirlineDate(String aAirlineDate) {
+   airlineDate= aAirlineDate;
+}
+ public String getAirlineDate() {
+   return airlineDate;
+}
+
+
+public void setPassengerName(String aPassengerName) {
+   passengerName= aPassengerName;
+}
+ public String getPassengerName() {
+   return passengerName;
+}
+
+
+ public void setGender(String aGender) {
+   gender= aGender;
+}
+ public String getGender() {
+   return gender;
+}
+
+
+ public void setDateOfBirth(String aDateOfBirth) {
+   dateOfBirth= aDateOfBirth;
+}
+ public String getDateOfBirth() {
+   return dateOfBirth;
+}
+
+
+ public void setFare(String aFare) {
+   fare= aFare;
+}
+ public String getFare() {
+   return fare;
+}
+
+
+ public void setDepartureTime(String aDepartureTime) {
+   departureTime= aDepartureTime;
+}
+ public String getDepartureTime() {
+   return departureTime;
+}
+
+
+ public void setSeatClass(String aSeatClass) {
+   seatClass= aSeatClass;
+}
+ public String getSeatClass() {
+   return seatClass;
+}
+
+ public void setMobileNumber(String aMobileNumber) {
+   mobileNumber= aMobileNumber;
+}
+ public String getMobileNumber() {
+   return mobileNumber;
+}
+
+ public void setPaymentOption(String aPaymentOption) {
+   paymentOption= aPaymentOption;
+}
+ public String getPaymentOption() {
+   return paymentOption;
+}
+
+ public void setCardNo(String aCardNo) {
+   cardNo= aCardNo;
+}
+ public String getCardNo() {
+   return cardNo;
+}
+
+ public void setCvvNo(String aCvvNo) {
+   cvvNo= aCvvNo;
+}
+ public String getCvvNo() {
+   return cvvNo;
+}
+
+ public void setChequeNo(String aChequeNo) {
+   chequeNo= aChequeNo;
+}
+ public String getChequeNo() {
+   return chequeNo;
+}
+
+ public void setAddress(String aAddress) {
+   address= aAddress;
+}
+ public String getAddress() {
+   return address;
+}
+
+ public void setNotes(String aNotes) {
+   notes= aNotes;
+}
+ public String getNotes() {
+   return notes;
+}
+}
